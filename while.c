13c64e1b87b08f61ed2cbdf3b020dfdbfd1b8264
Nakul MR <nakulmr289@gmail.com>
2026-07-04 21:04:17 +0530

    added new program

---
+++ while.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,diff;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a,&b);
+    while (1)
+    {
+        
+            diff=a-b;
+        
+        
+    
+     printf("difference = %d",diff);
+     break;
+    }
+    return 0;
+}
